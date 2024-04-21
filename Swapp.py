f4bb740eae506f8e794987e6b91f9dd3202d2116
Darxx01 <167700537+Darxx01@users.noreply.github.com>
2024-04-21 12:23:07 -0500

    Create Swapp.py

---
+++ Swapp.py
@@ -0,0 +1,34 @@
+import requests
+
+data = requests.get('https://swapi.dev/api/films').json()
+
+peliculas = 0
+for i in data['results']:
+  for j in i['planets']:
+    res = requests.get(j).json()
+    if res['climate'] == 'arid':
+      peliculas += 1
+      break
+
+
+wookies = 0
+for i in data['results'][-1]['characters']:
+  res = requests.get(i).json()
+  if 'https://swapi.dev/api/species/3/' in res['species']:
+    wookies += 1
+
+
+aeronave = {'nombre':'', 'largo':0}
+data = requests.get('https://swapi.dev/api/starships/').json()
+for i in data['results']:
+  largo = float(i['length'].replace(',','.'))
+  if aeronave['largo'] < largo:
+    aeronave['nombre'] = i['name']
+    aeronave['largo'] = largo
+
+print('¿En cuántas películas aparecen planetas cuyo clima sea árido?')
+print(peliculas)
+print('¿Cuántos Wookies aparecen en la sexta película?')
+print(wookies)
+print('¿Cuál es el nombre de la aeronave más grande en toda la saga?')
+print(aeronave)
